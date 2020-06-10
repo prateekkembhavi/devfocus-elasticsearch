ba32ab1c8202458f0b0cadb6da4b2dfac50fa2eb
Prateek.Kembhavi@ge.com <Prateek.Kembhavi@ge.com>
2020-06-10 19:12:45 +0530

    content type in setup.sh

--- chapter3/setup.sh
+++ chapter3/setup.sh
@@ -1,3 +1,3 @@
 curl -XDELETE http://localhost:9200/pokedex
 curl -XPUT http://localhost:9200/pokedex -H 'Content-Type: application/json' -d @setup.json
-curl -s -XPUT http://localhost:9200/_bulk 'Content-Type: application/json' --data-binary @pokemon_bulk.json
+curl -s -XPUT http://localhost:9200/_bulk -H 'Content-Type: application/json' --data-binary @pokemon_bulk.json
